781265ddd768ea2e3804806eea94ee9c6aab1731
Ulises Peguero <peguero.ulises@gmail.com>
2023-04-03 23:33:28 -0500

    Initial setup PBS API

---
+++ src/main/java/com/portfolio/api/pbsbackend/PbsBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.api.pbsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbsBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PbsBackEndApplication.class, args);
+	}
+
+}
