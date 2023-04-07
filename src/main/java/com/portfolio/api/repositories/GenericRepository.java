95452e20db2bf0eb19030a93d4c593bbc14a7783
Ulises Peguero <peguero.ulises@gmail.com>
2023-04-06 23:33:38 -0500

    Added pagination to the GenericService/Controller

--- src/main/java/com/portfolio/api/repositories/GenericRepository.java
+++ src/main/java/com/portfolio/api/repositories/GenericRepository.java
@@ -2,6 +2,7 @@ package com.portfolio.api.repositories;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
@@ -10,4 +11,6 @@ import com.portfolio.api.models.AbstractEntity;
 @NoRepositoryBean
 public interface GenericRepository<T extends AbstractEntity> extends JpaRepository<T, Long> {
   public List<T> findByActive(Boolean active);
+
+  public List<T> findByActive(Boolean active, Pageable pageable);
 }
--- src/main/java/com/portfolio/api/services/GenericService.java
+++ src/main/java/com/portfolio/api/services/GenericService.java
@@ -2,6 +2,12 @@ package com.portfolio.api.services;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
 import com.portfolio.api.models.AbstractEntity;
 import com.portfolio.api.repositories.GenericRepository;
 
@@ -13,8 +19,29 @@ public abstract class GenericService<T extends AbstractEntity> {
     this.repository = repository;
   }
 
+  public Page<T> findAll(Integer pageNumber, Integer rowsPerPage, String orderBy, String order) {
+    Pageable page;
+    Sort sort = getSorter(orderBy, order);
+    page = PageRequest.of(pageNumber, rowsPerPage == null ? 15 : rowsPerPage, sort);
+    return repository.findAll(page);
+  }
+
+  public List<T> findAll(String orderBy, String order) {
+    Sort sort = getSorter(orderBy, order);
+    return repository.findAll(sort);
+  }
+
   public List<T> findAll() {
     return repository.findAll();
+  }
+
+  protected Sort getSorter(String orderBy, String order) {
+    Sort sort = Sort.unsorted();
+    if (orderBy != null || order != null) {
+      sort = Sort.by(orderBy);
+      sort = "DESC".equalsIgnoreCase(order) ? sort.descending() : sort.ascending();
+    }
+    return sort;
   }
 
 }
