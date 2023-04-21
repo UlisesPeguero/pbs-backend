f640a4598f9407fa345d28f02b593ba36ceacdce
Ulises Fox Peguero <peguero.ulises@gmail.com>
2023-04-20 21:30:31 -0500

    Added validation check for post and put

--- src/main/java/com/portfolio/api/controllers/GenericController.java
+++ src/main/java/com/portfolio/api/controllers/GenericController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.portfolio.api.controllers.data.MessagePayload;
 import com.portfolio.api.exceptions.FailedValidationException;
@@ -16,12 +19,7 @@ import com.portfolio.api.models.GenericEntity;
 import com.portfolio.api.repositories.GenericRepository;
 import com.portfolio.api.services.GenericService;
 
-import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.Valid;
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 public abstract class GenericController<T extends GenericEntity<T>> {
 
@@ -58,16 +56,14 @@ public abstract class GenericController<T extends GenericEntity<T>> {
   }
 
   @PostMapping
-  public ResponseEntity<T> create(@Valid @RequestBody T newItem, Errors errors) {
-    try {
-      return ResponseEntity.ok(service.create(newItem));
-    } catch (ConstraintViolationException exception) {
-      throw new FailedValidationException(errors);
-    }
+  public ResponseEntity<T> create(@Valid @RequestBody T newItem, Errors validation) {
+    checkForValidationErrors(validation);
+    return ResponseEntity.ok(service.create(newItem));
   }
 
   @PutMapping("/{id}")
-  public ResponseEntity<T> update(@PathVariable Long id, @RequestBody T updatedItem) {
+  public ResponseEntity<T> update(@PathVariable Long id, @Valid @RequestBody T updatedItem, Errors validation) {
+    checkForValidationErrors(validation);
     return ResponseEntity.ok(service.update(id, updatedItem));
   }
 
@@ -75,6 +71,11 @@ public abstract class GenericController<T extends GenericEntity<T>> {
   public ResponseEntity<MessagePayload> delete(@PathVariable Long id) {
     service.delete(id);
     return ResponseEntity.ok(new MessagePayload("Resource has been deleted."));
+  }
+
+  protected void checkForValidationErrors(Errors validation) {
+      if(validation.hasErrors())
+        throw new FailedValidationException(validation);
   }
 
 }
