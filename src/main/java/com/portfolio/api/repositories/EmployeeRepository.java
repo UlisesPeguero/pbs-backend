182b45fb3318ecfa3f961297bbdeec0aea7da759
Tigioz <z.fouts@icloud.com>
2023-05-08 16:04:19 -0500

    created employee repository

--- src/main/java/com/portfolio/api/repositories/EmployeeRepository.java
+++ src/main/java/com/portfolio/api/repositories/EmployeeRepository.java
@@ -1,2 +1,6 @@
-package com.portfolio.api.repositories;public interface EmployeeRepository {
+package com.portfolio.api.repositories;
+
+import com.portfolio.api.models.Employee;
+
+public interface EmployeeRepository extends GenericRepository<Employee>{
 }
